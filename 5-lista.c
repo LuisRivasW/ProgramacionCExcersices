b60c47365220830055d2cca47a9ef02d6685850c
María Eugenia Tirado <mariatiperez@gmail.com>
2018-11-04 20:18:23 -0400

    Crear, insertal al final, imprimir

--- 5-lista.c
+++ 5-lista.c
@@ -2,5 +2,12 @@
 #include <stdio.h>
 #include "lista.h"
 int main () {
-    
+    node * np = crearNodo(5), * lp = NULL;
+    insertarFinal(&lp, &np);
+    np = crearNodo(6);
+    insertarFinal(&lp,  &np);
+    np = crearNodo(8);
+    insertarFinal(&lp,  &np);
+    imprimir(lp);
+
 }
--- lista.h
+++ lista.h
@@ -5,3 +5,32 @@ typedef struct Node {
     int valor;
     struct Node * sig;
 } node;
+
+
+node * crearNodo( int valor) {
+    node * auxp = (node *)malloc(sizeof(node));
+    auxp->valor = valor;
+    auxp->sig = NULL;
+    return auxp;
+}
+
+
+void insertarFinal(node **lp, node **np){
+    
+    if (*lp == NULL) {
+        *lp = *np;
+        return;
+    }
+    node * auxp;
+    for(auxp= *lp; auxp->sig != NULL; auxp=auxp->sig);
+    auxp->sig = *np;
+    return;
+
+}
+
+void imprimir(node *lp) {
+    node * auxp;
+    for(auxp=lp; auxp != NULL; auxp=auxp->sig)
+        printf("%i->", auxp->valor);;
+    printf("NULL\n");
+}
